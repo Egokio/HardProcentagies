ade1a460ba127edfd19aabef18843389d313a02b
Egokio <mikhaylov@ut.ee>
2026-09-13 18:43:20 +0300

    Добавьте файлы проекта.

---
+++ HardProcentagies/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Text;
+
+namespace HardProcentagies
+{
+    public class Calculator
+    {
+        public static (double, double, double) CountPercentage(double rate, double money,  int periodMonth)
+        {
+            double totalMoney = money * Math.Pow((1 + (rate / 1200)), periodMonth);
+            double income = totalMoney - money;
+            double incomePercentage = income / money * 100;
+
+            return (Math.Round(totalMoney, 2), Math.Round(incomePercentage, 2), Math.Round(income, 2));
+        }
+    }
+}
+
---
+++ HardProcentagies/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HardProcentagies
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Money_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Period_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Rate_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Computing_Click(object sender, RoutedEventArgs e)
+        {
+            if (!double.TryParse(Money.Text, out double moneyInput) || moneyInput <= 0)
+            {
+               
+                    MessageBox.Show("Type correct amount!");
+                    return;
+                
+            }
+
+            if (!int.TryParse(PeriodMonths.Text, out int periodMonthsInput) || periodMonthsInput <= 0)
+            {
+             
+                    MessageBox.Show("Wrong month input");
+                    return;
+                
+            }
+            if (!double.TryParse(Rate.Text, out double rateInput) || rateInput <= 0)
+            {
+
+                    MessageBox.Show("Wrong rate input");
+                    return;
+                
+            }
+            var (totalMoney, incomePercentage, income) = Calculator.CountPercentage(rateInput, moneyInput, periodMonthsInput);
+            TotalAmount.Content = totalMoney.ToString();
+            PercentIncome.Content = incomePercentage.ToString();
+            MoneyIncome.Content = income.ToString();
+        }
+    }
+}
